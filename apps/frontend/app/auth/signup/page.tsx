ff6df5e197677c4f1e70698ddbe8586185a34ad5
Chathusha Mendis <chathushamendis@icloud.com>
2025-07-16 20:49:18 +0530

    feat: add signin and signup pages with forms and styling

---
+++ apps/frontend/app/auth/signup/page.tsx
@@ -0,0 +1,133 @@
+import Image from "next/image";
+
+export default function SignupPage() {
+  return (
+    <>
+      <div className="flex min-h-full flex-1">
+        <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+          <div className="mx-auto w-full max-w-sm lg:w-96">
+            <div>
+              <Image
+                alt="Anything.lk"
+                src="/logo.png"
+                className="h-32 w-auto"
+                width={500}
+                height={500}
+              />
+              <h2 className="mt-8 text-2xl/9 font-bold tracking-tight text-gray-900">
+                Create your free account
+              </h2>
+              <p className="mt-2 text-sm/6 text-gray-500">
+                Already a member?{" "}
+                <a
+                  href="/auth/signin"
+                  className="font-semibold text-vivid-magenta hover:text-vivid-magenta-hover"
+                >
+                  Signin here
+                </a>
+              </p>
+            </div>
+
+            <div className="mt-10">
+              <div>
+                <form action="#" method="POST" className="space-y-6">
+                  <div>
+                    <label
+                      htmlFor="first-name"
+                      className="block text-sm/6 font-medium text-gray-900"
+                    >
+                      First name
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="first-name"
+                        name="first-name"
+                        type="text"
+                        required
+                        autoComplete="given-name"
+                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="last-name"
+                      className="block text-sm/6 font-medium text-gray-900"
+                    >
+                      Last name
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="last-name"
+                        name="last-name"
+                        type="text"
+                        required
+                        autoComplete="family-name"
+                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm/6 font-medium text-gray-900"
+                    >
+                      Email address
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        required
+                        autoComplete="email"
+                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="password"
+                      className="block text-sm/6 font-medium text-gray-900"
+                    >
+                      Password
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        required
+                        autoComplete="current-password"
+                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <button
+                      type="submit"
+                      className="flex w-full justify-center rounded-md bg-vivid-magenta px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-vivid-magenta-hover focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      Sign up
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="relative hidden w-0 flex-1 lg:block">
+          <img
+            alt=""
+            src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+            className="absolute inset-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
