a1b48d31d2155b45154952c47a341dee4d01abd2
Chathusha Mendis <chathushamendis@icloud.com>
2025-07-21 20:15:01 +0530

    refactor: removed address form my account

--- apps/backend/src/java/controller/MyAccount.java
+++ apps/backend/src/java/controller/MyAccount.java
@@ -2,24 +2,18 @@ package controller;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import hibernate.Address;
-import hibernate.City;
 import hibernate.HibernateUtil;
 import hibernate.User;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import model.Util;
-import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Restrictions;
 
 @WebServlet(name = "MyAccount", urlPatterns = {"/MyAccount"})
 public class MyAccount extends HttpServlet {
@@ -43,23 +37,18 @@ public class MyAccount extends HttpServlet {
             String since = new SimpleDateFormat("MMM yyyy").format(user.getCreated_at());
             respoJsonObject.addProperty("since", since);
             
-            Session s = HibernateUtil.getSessionFactory().openSession();
-            Criteria c = s.createCriteria(Address.class);
-            
-            c.add(Restrictions.eq("user", user));
-            if (!c.list().isEmpty()) {
-                
-                @SuppressWarnings("unchecked")
-                List<Address> addressList = c.list();
-                
-                respoJsonObject.add("addressList", gson.toJsonTree(addressList));
-                
-            }
-            
             String toJson = gson.toJson(respoJsonObject);
             response.setContentType("application/json");
             response.getWriter().write(toJson);
             
+        } else {
+            
+            JsonObject responseObject = new JsonObject();
+            responseObject.addProperty("status", false);
+            responseObject.addProperty("message", "Please sign in to continue");
+            
+            response.setContentType("application/json");
+            response.getWriter().write(new Gson().toJson(responseObject));
         }
         
     }
@@ -72,10 +61,6 @@ public class MyAccount extends HttpServlet {
         
         String firstName = userData.get("firstName").getAsString();
         String lastName = userData.get("lastName").getAsString();
-        String lineOne = userData.get("lineOne").getAsString();
-        String lineTwo = userData.get("lineTwo").getAsString();
-        String postalCode = userData.get("postalCode").getAsString();
-        int cityId = userData.get("cityId").getAsInt();
         
         JsonObject responseObject = new JsonObject();
         responseObject.addProperty("status", false);
@@ -84,67 +69,49 @@ public class MyAccount extends HttpServlet {
             responseObject.addProperty("message", "First name can not be empty!");
         } else if (lastName.isEmpty()) {
             responseObject.addProperty("message", "Last name can not be empty!");
-        } else if (lineOne.isEmpty()) {
-            responseObject.addProperty("message", "Enter address line one");
-        } else if (lineTwo.isEmpty()) {
-            responseObject.addProperty("message", "Enter address line two");
-        } else if (postalCode.isEmpty()) {
-            responseObject.addProperty("message", "Enter your postal code");
-        } else if (!Util.isCodeValid(postalCode)) {
-            responseObject.addProperty("message", "Enter correct postal code");
-        } else if (cityId == 0) {
-            
-            responseObject.addProperty("message", "Select a city");
-            
         } else {
             
-            HttpSession ses = request.getSession();
+            HttpSession ses = request.getSession(false);
             
-            if (ses.getAttribute("user") != null) {
+            if (ses == null || ses.getAttribute("user") == null) {
+                responseObject.addProperty("message", "Please sign in to continue");
+            } else {
                 
                 User u = (User) ses.getAttribute("user");  // get session user
 
                 SessionFactory sf = HibernateUtil.getSessionFactory();
                 Session s = sf.openSession();
                 
-                User u1 = (User) s.get(User.class, u.getId());
+                try {
+                    s.beginTransaction();
+                    
+                    User u1 = (User) s.get(User.class, u.getId());
 
-                if (u1 != null) {
-                    
-                    u1.setFirst_name(firstName);
-                    u1.setLast_name(lastName);
-                    
-                    City city = (City) s.load(City.class, cityId);   // primary key search
-                    
-                    Criteria addressCriteria = s.createCriteria(Address.class);
-                    addressCriteria.add(Restrictions.eq("user", u1));
-                    Address address;
-                    if(addressCriteria.list().isEmpty()){
-                        address = new Address();
-                    }else{
-                        address = (Address) addressCriteria.list().get(0);
+                    if (u1 != null) {
+                        
+                        u1.setFirst_name(firstName);
+                        u1.setLast_name(lastName);
+
+                        // session-management
+                        ses.setAttribute("user", u1);
+                        // session-management
+
+                        s.merge(u1);
+                        s.getTransaction().commit();
+                        
+                        responseObject.addProperty("status", true);
+                        responseObject.addProperty("message", "User profile details updated successfully!");
+                        
+                    } else {
+                        responseObject.addProperty("message", "User not found");
                     }
                     
-                    address.setLineOne(lineOne);
-                    address.setLineTwo(lineTwo);
-                    address.setPostalCode(postalCode);
-                    address.setCity(city);
-                    address.setUser(u1);
-
-                    // session-management
-                    ses.setAttribute("user", u1);
-                    // session-management
-
-                    s.merge(u1);
-                    s.saveOrUpdate(address);
-                    
-                    s.beginTransaction().commit();
-                    responseObject.addProperty("status", true);
-                    responseObject.addProperty("message", "User profile details update successfully!");
+                } catch (Exception e) {
+                    s.getTransaction().rollback();
+                    responseObject.addProperty("message", "Error updating profile: " + e.getMessage());
+                } finally {
                     s.close();
-                    
                 }
-                
             }
             
         }
