dbbe15e5d546a91ca738e68ad6fcd1da2bc9550a
Chathusha Mendis <chathushamendis@icloud.com>
2025-07-23 10:22:10 +0530

    fix: add additional remote pattern for Tailwind CSS images in next.config.ts

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "tailwindcss.com",
+        port: "",
+        pathname: "/plus-assets/img/ecommerce-images/**",
+      },
     ],
   }
 };
